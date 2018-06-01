5dbd64c6f3dbeaee86dd1f6e1f0fd5edc72e17d0
Viktoria Kozopas <kozopasv@gmail.com>
2018-06-01 20:35:08 +0300

    start working with moving platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public Vector3 MoveBy;
+    Vector3 pointA;
+    Vector3 pointB;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.pointA = this.transform.position;
+        this.pointB = this.pointA + MoveBy;
+    }
+
+    bool isArrived(Vector3 pos, Vector3 target)
+    {
+        pos.z = 0;
+        target.z = 0;
+        return Vector3.Distance(pos, target) < 0.02f;
+    }
+}
