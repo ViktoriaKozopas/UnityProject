d5a1d9576a50e9a7729bf0e6e888aa0a73b9c719
Viktoria Kozopas <kozopasv@gmail.com>
2018-06-03 15:46:58 +0300

    platform is moving
    platform if moving up and down

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
 
-    public Vector3 MoveBy;
+    //public Vector3 MoveBy;
     Vector3 pointA;
     Vector3 pointB;
+    Vector3 nextPosition;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform childTransform;
+    [SerializeField]
+    private Transform transformB;
 
     // Use this for initialization
     void Start()
     {
-        this.pointA = this.transform.position;
-        this.pointB = this.pointA + MoveBy;
+        this.pointA = childTransform.localPosition;
+        this.pointB = transformB.localPosition;
+        this.nextPosition = pointB;
     }
 
-    bool isArrived(Vector3 pos, Vector3 target)
+    void Update()
     {
-        pos.z = 0;
-        target.z = 0;
-        return Vector3.Distance(pos, target) < 0.02f;
+        move();   
     }
+
+    private void move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPosition, speed*Time.deltaTime);
+
+        if (Vector3.Distance(childTransform.localPosition, nextPosition) < 0.02f)
+        {
+            changeDestination();
+        }
+    }
+
+    private void changeDestination()
+    {
+        nextPosition = nextPosition == pointA ? pointB : pointA;
+    }
+
+    //bool isArrived(Vector3 pos, Vector3 target)
+    //{
+    //    pos.z = 0;
+    //    target.z = 0;
+    //    return Vector3.Distance(pos, target) < 0.02f;
+    //}
 }
