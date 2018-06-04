d8a37f59d1da81f4e4355254df309709912268fc
Viktoria Kozopas <kozopasv@gmail.com>
2018-06-04 14:01:46 +0300

    add death animation and edit scene

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -24,7 +24,7 @@ public class MovingPlatform : MonoBehaviour {
         this.nextPosition = pointB;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         move();   
     }
@@ -55,14 +55,4 @@ public class MovingPlatform : MonoBehaviour {
         platformTimer = 0;
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    collision.gameObject.layer = 8;
-    //    collision.transform.SetParent(childTransform);
-    //}
-
-    //private void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    collision.transform.SetParent(null);
-    //}
 }
