9b46f0761efa08943b2154a7b8f589377b693ef1
Viktoria Kozopas <kozopasv@gmail.com>
2018-06-19 15:33:58 +0300

    orc goes toward rabbit when it in orc's zone

--- Assets/Scripts/OrcGreen.cs
+++ Assets/Scripts/OrcGreen.cs
@@ -7,11 +7,12 @@ public class OrcGreen : MonoBehaviour {
 
     public Transform transformA;
     public Transform transformB;
+    public Transform rabbit;
 
     Rigidbody2D orcBody;
     SpriteRenderer orcSprite;
 
-    Vector3 rabbit_position;
+    Vector3 rabbit_position; // for attack? if no use - delete
 
     public enum Mode
     {
@@ -31,12 +32,13 @@ public class OrcGreen : MonoBehaviour {
     private void Start()
     {
 
-        rabbit_position = HeroRabbit.lastRabbit.transform.position;
+//        rabbit_position = HeroRabbit.lastRabbit.transform.position;
         mode = Mode.GoToA;
     }
 
     void FixedUpdate()
     {
+        //orc move from point A to point B
         orcSprite.flipX = mode == Mode.GoToB ? true : false;
         if (gameObject.transform.position.x < transformA.position.x)
         {
@@ -54,6 +56,18 @@ public class OrcGreen : MonoBehaviour {
         {
             orcBody.velocity = new Vector2(-speed, orcBody.velocity.y);
         }
-    }
+        // if rabbit in green orc zone
+        if (HeroRabbit.lastRabbit.transform.position.x > Mathf.Min(transformA.position.x, transformB.position.x) &&
+            HeroRabbit.lastRabbit.transform.position.x < Mathf.Max(transformA.position.x, transformB.position.x))
+        {
+            mode = Mode.Attack;
+        }
 
+        if(mode == Mode.Attack)
+        {
+            Debug.Log("Attack");
+            transform.position = Vector2.MoveTowards(gameObject.transform.position, rabbit.transform.position, speed * Time.deltaTime);
+        }
+
+    }
 }
